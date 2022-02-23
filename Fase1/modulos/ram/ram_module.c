a31e809fcef922645a8955bb6f30f4ae1908a53c
Erick Antillon <jpj007.ek@gmail.com>
2022-02-23 01:40:38 -0600

    Iniciado modulo de ram

---
+++ Fase1/modulos/ram/ram_module.c
@@ -0,0 +1,54 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/proc_fs.h>
+#include <linux/sched.h>
+#include <linux/uaccess.h>
+#include <linux/fs.h>
+#include <linux/sysinfo.h>
+#include <linux/seq_file.h>
+#include <linux/slab.h>
+#include <linux/mm.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Grupo 6");
+MODULE_DESCRIPTION("Modulo de lectura de RAM");
+MODULE_VERSION("1.0.0");
+
+struct sysinfo info;
+
+static int escribir_a_proc(struct seq_file *file_proc, void *v)
+{
+    si_meminfo(&info);
+    long unsigned int totalram = ((uint64_t)info.totalram * info.mem_unit) / (1024 * 1024);
+    long unsigned int freeram = ((uint64_t)info.freeram * info.mem_unit) / (1024 * 1024);
+    long unsigned int ramusage = totalram - freeram;
+    float percent = totalram * ramusage/100; 
+    seq_printf(file_proc, "{\"totalram\": %lu,\"freeram\":%lu, \"ramusage\": %lu}", totalram, freeram, ramusage);
+
+    return 0;
+}
+
+static int abrir_aproc(struct inode *inode, struct file *file)
+{
+    return single_open(file, escribir_a_proc, NULL);
+}
+
+static struct proc_ops archivo_operaciones = {
+    .proc_open = abrir_aproc,
+    .proc_read = seq_read};
+
+static int __init ram_module_init(void)
+{
+    proc_create("ram_module", 0, NULL, &archivo_operaciones);
+    printk(KERN_INFO "Modulo de RAM insertado \n");
+    return 0;
+}
+
+static void __exit ram_module_cleanup(void)
+{
+    remove_proc_entry("ram_module", NULL);
+    printk(KERN_INFO "Modulo de RAM extraido\n");
+}
+
+module_init(ram_module_init);
+module_exit(ram_module_cleanup);
