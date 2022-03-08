b777d4de54b75f70e0c412ad0070154ffacf16dc
Evelyn Alejandra Navarro Ozorio <3302179251202@ingenieria.usac.edu.gt>
2022-03-08 14:52:14 -0600

    corrección servidor

--- Fase1/Server/main.go
+++ Fase1/Server/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,10 +12,17 @@ import (
 
 type InfoRam struct {
 	Vm             string `json:"Vm"`
-	Total_ram      int    `json:"Total_ram"`
-	Ram_usada      int    `json:"Ram_usada"`
-	Porcentaje_uso int    `json:"Porcentaje_uso"`
-	Ram_libre      int    `json:"Ram_libre"`
+	Total_ram      int    `json:"Totalram"`
+	Ram_usada      int    `json:"Ramusage"`
+	Porcentaje_uso int    `json:"Rampercent"`
+	Ram_libre      int    `json:"Freeram"`
+}
+
+type ramgenerada struct {
+	Totalram   int `json:"Totalram"`
+	Ramusage   int `json:"Ramusage"`
+	Rampercent int `json:"Rampercent"`
+	Freeram    int `json:"Freeram"`
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -23,45 +32,42 @@ func home(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Proyecto 1 Grupo 6")
 }
 
-func ram(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-
-}
-
-func procesos(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-}
-
 func getram(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
-	SendRam := InfoRam{
-		Vm:             "oa",
-		Total_ram:      0,
-		Ram_usada:      0,
-		Porcentaje_uso: 0,
-		Ram_libre:      0,
+	ram := "/proc/ram_module"
+	bytesLeidos, err2 := ioutil.ReadFile(ram)
+	if err2 != nil {
+		log.Println(err2)
+		return
 	}
-	json.NewEncoder(w).Encode(SendRam)
+
+	leido := string(bytesLeidos[:])
+
+	json.NewEncoder(w).Encode(string(leido))
 }
 
 func getprocesos(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	cpu := "/proc/process_module"
+	bytesLeidos, err2 := ioutil.ReadFile(cpu)
+	if err2 != nil {
+		log.Println(err2)
+		return
+	}
+
+	leido := string(bytesLeidos[:])
+
+	json.NewEncoder(w).Encode(string(leido))
 }
 
 func main() {
 	router := mux.NewRouter()
 	http.HandleFunc("/", home)
-	router.HandleFunc("/ram", ram).Methods("GET", "OPTIONS")
-	router.HandleFunc("/procesos", procesos).Methods("GET", "OPTIONS")
 	router.HandleFunc("/getram", getram).Methods("GET", "OPTIONS")
 	router.HandleFunc("/getprocesos", getprocesos).Methods("GET", "OPTIONS")
 	fmt.Println("Server running on port 5000")
