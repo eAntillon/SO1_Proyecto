89bec21570aac0bb8df1fb7a3e801f55c9dc489a
Josue David Zea Herrera <69376961+Josue-Zea@users.noreply.github.com>
2022-03-09 23:35:54 -0600

    Sockets implementation

--- Fase1/frontend/src/components/Screens/ApiReports.js
+++ Fase1/frontend/src/components/Screens/ApiReports.js
@@ -1,33 +1,93 @@
-import React from 'react';
-//import { io } from "socket.io-client";
-//const socket = io.connect(process.env.REACT_APP_SOCKETS);
-export const ApiReports = () => {
-  const seeProcess = async () => {
-    let ruta = `http://${process.env.REACT_APP_API}/getprocesos`;
-    let result = await fetch(ruta)
-    .then((response) => {
-        return response.json();
-    }).catch(() => {
-      console.log("Error en solicitud a las vm procesos");
-      return "";
-    });
-    console.log(result);
-  };
-    /*useEffect(() => {
-    socket.on("message", ( object ) => {
-      console.log( object );
-    });
-  });*/
+import React, { useEffect, useState } from 'react';
+import { Accordion } from 'react-bootstrap';
 
-      /*console.log(result);
-    const data = {
-      nombre: "Ana Jeanete Flores Rivera"
-    }*/
-    //socket.emit("message", data);
+import { io } from "socket.io-client";
+const socket = io.connect(process.env.REACT_APP_SOCKETS);
+
+export const ApiReports = () => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    socket.emit("getlogs");
+  }, []);
+
+  socket.on("sendlogs", (object) => {
+    setData(object);
+  });
 
   return (
     <div>
-      
+      <Accordion defaultActiveKey="0" className="Accordion_1">
+        {
+          data.map((item, index) => {
+            return (
+              <Accordion.Item eventKey={index + 1}>
+                <Accordion.Header>
+                  <table className="table">
+                    <tbody>
+                      <td>
+                        Maquina: {item.nombreVM}
+                      </td>
+                      <td>
+                        Endpoint: {item.endpoint}
+                      </td>
+                      <td>
+                        Fecha: {item.date}
+                      </td>
+                    </tbody>
+                  </table>
+                </Accordion.Header>
+                {
+                  <Accordion.Body>
+                    {
+                      item.endpoint === "/getram" ?
+                        <table className="table">
+                          <tbody>
+                            <td>
+                              Ram: {item.data.totalram}
+                            </td>
+                            <td>
+                              Porcentaje: {item.data.rampercent}
+                            </td>
+                            <td>
+                              En uso: {item.data.ramusage}
+                            </td>
+                            <td>
+                              Libre: {item.data.freeram}
+                            </td>
+                          </tbody>
+                        </table>
+                        :
+                        <table className="table">
+                          {
+                            item.data.slice(0, 10).map(child => {
+                              return (
+                                <tbody>
+                                  <td>
+                                    Id proceso: {child.process_id}
+                                  </td>
+                                  <td>
+                                    Nombre proceso: {child.process_name}
+                                  </td>
+                                  <td>
+                                    PID_padre: {child.parent_id}
+                                  </td>
+                                </tbody>
+                              )
+                            })
+                          }
+                        </table>
+                    }
+                  </Accordion.Body>
+                }
+              </Accordion.Item>
+            )
+          })
+        }
+      </Accordion>
     </div>
   )
 }
+
+/*
+
+*/
---
+++ Fase1/sockets/app.js
@@ -0,0 +1,25 @@
+const server = require('http').createServer();
+require("dotenv").config();
+let user = process.env.USR;
+let pass = process.env.PASS;
+let host = process.env.HOST;
+
+//Coneccion con mongo
+const uri = `mongodb://${user}:${pass}@${host}`;
+const { MongoClient } = require('mongodb');
+
+const io = require('socket.io')(server,{ cors: {
+  origin: "*"
+}});
+
+io.on('connection', async (client) => {
+  client.on('getlogs', async ( ) => {
+    const cliente = new MongoClient(uri);
+    let connection = await cliente.connect();
+    let db = await connection.db("Proyecto");
+    let collection = await db.collection("logs");
+    let data = await collection.find({}).sort({$natural:-1}).limit(30).toArray();
+    io.emit("sendlogs", data);
+  });
+});
+server.listen(8080);
